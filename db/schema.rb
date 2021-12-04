afd3fb8484962868814c98088d95ee1e8fe32aaf
shiku-booth <sbarathi@chicagobooth.edu>
2021-12-04 01:24:09 +0000

    migrate user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_04_012142) do
+ActiveRecord::Schema.define(version: 2021_12_04_012330) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "author_id"
@@ -41,6 +41,17 @@ ActiveRecord::Schema.define(version: 2021_12_04_012142) do
     t.string "image"
     t.integer "likes_count"
     t.integer "owner_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.integer "comments_count"
+    t.string "email"
+    t.integer "likes_count"
+    t.string "password_digest"
+    t.boolean "private"
+    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
