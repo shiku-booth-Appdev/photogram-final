1bfc620a2e823a19399b64920f40bf0890a32d13
shiku-booth <sbarathi@chicagobooth.edu>
2021-12-04 01:22:14 +0000

    like migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_04_012049) do
+ActiveRecord::Schema.define(version: 2021_12_04_012142) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "author_id"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 2021_12_04_012049) do
     t.integer "recipient_id"
     t.integer "sender_id"
     t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "fan_id"
+    t.integer "photo_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
