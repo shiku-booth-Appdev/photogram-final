0e124cf94360b24f8d4c02d796e5936739fcad4a
shiku-booth <sbarathi@chicagobooth.edu>
2021-12-04 01:20:32 +0000

    migrate comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_04_011758) do
+ActiveRecord::Schema.define(version: 2021_12_04_011948) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "author_id"
+    t.text "body"
+    t.integer "photo_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "photos", force: :cascade do |t|
     t.text "caption"
