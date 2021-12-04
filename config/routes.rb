395fe6a46b36cdd0bcf99e0d8a78e7ea2ad116f1
shiku-booth <sbarathi@chicagobooth.edu>
2021-12-04 01:19:19 +0000

    migrate photo

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,32 @@
 Rails.application.routes.draw do
+  # Routes for the Photo resource:
+
+  # CREATE
+  post("/insert_photo", { :controller => "photos", :action => "create" })
+          
+  # READ
+  get("/photos", { :controller => "photos", :action => "index" })
+  
+  get("/photos/:path_id", { :controller => "photos", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_photo/:path_id", { :controller => "photos", :action => "update" })
+  
+  # DELETE
+  get("/delete_photo/:path_id", { :controller => "photos", :action => "destroy" })
+
+  #------------------------------
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
+
+# rails generate draft:resource photo caption:text comments_count:integer image:string likes_count:integer owner_id:integer
+# rails generate draft:resource comment author_id:integer body:text photo_id:integer
+# rails generate draft:resource followrequest recipient_id:integer sender_id:integer status:string
+# rails generate draft:resource like fan_id:integer photo_id:integer
+
+# rails generate draft:account user comments_count:integer email:string likes_count:integer password_digest:string private:boolean username:string
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_12_04_011758) do
+
+  create_table "photos", force: :cascade do |t|
+    t.text "caption"
+    t.integer "comments_count"
+    t.string "image"
+    t.integer "likes_count"
+    t.integer "owner_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
